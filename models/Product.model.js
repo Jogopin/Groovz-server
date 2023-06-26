4689e7939756ad4d00b3c51c13296d77d0a10bc4
jogopin <jonn.g.pineda@gmail.com>
2023-06-26 11:14:15 +0200

    Add Product Schema

---
+++ models/Product.model.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose")
+
+const productSchema = new Schema(
+    {
+        name: {
+            type:String,
+            required:[true, "name is required"],
+            trim:true
+        },
+        reference:{
+            type:String,
+            required:[true,"reference is required"],
+            trim:true,
+            unique:true,
+        },
+        images:[String],
+        rating:[{
+            value:{
+                type:Number,
+                required:[true,"value is required"],  
+            },
+            author:{
+                type: mongoose.Schema.Types.ObjectId,              
+                ref:"User"
+            },   
+            review:String,
+        }],
+        price:{
+            type:Number,
+            required:[true, "price is required"],
+        },
+        discount:{
+            type:Number,
+            default:0,
+        },
+        description:{
+            type:String,
+            required:[true, "description is required"],
+        },
+        stock:{
+            type:Number,
+            required:[true,"stock is required"]
+        }
+
+        
+    },
+    {
+        timestamps:true
+    }
+)
+const Product = model("Product", productSchema)
+
+module.exports=User
