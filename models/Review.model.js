58acd1bd2ef374f2832b48c7cd57f1f2f98e9fc5
jogopin <jonn.g.pineda@gmail.com>
2023-07-04 12:46:55 +0200

    Updated Review schema and added unique constraint

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,30 +1,31 @@
 const { Schema, model } = require("mongoose");
 
 const reviewSchema = new Schema(
-    {
-        product: {
-            type: Schema.Types.ObjectId,
-            ref: "Product",
-            required: true
-        },
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        },
-        rating: {
-            type: Number,
-            required: true
-        },
-        review: {
-            type: String,
-            required: true
-        }
+  {
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
     },
-    {
-        timestamps: true
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    rating: {
+      type: Number,
+      required: true
+    },
+    reviewText: {
+      type: String,
     }
+  },
+  {
+    timestamps: true
+  }
 );
+//  A user can only write one review for each product.
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 const Review = model("Review", reviewSchema);
 
