c632102c452dadbb5d4606eb3894be2349b8f279
jogopin <jonn.g.pineda@gmail.com>
2023-07-13 13:28:32 +0200

    Refactor '/checkout' route and enhance Stripe metadata

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,115 +1,103 @@
-const express = require("express")
+const express = require("express");
 const Product = require("../models/Product.model");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const { checkStock, formatLineItems } = require("../utils/checkoutHelpers");
 const router = express.Router();
 
 router.post("/checkout", (req, res, next) => {
-    const { productsToCheckout } = req.body;
+  const { productsToCheckout } = req.body;
 
-    // Respond early if no products were supplied in the request
-    if (productsToCheckout.length ===0){
-      res.status(400).json({message:"No items in the cart"})
-      return
-    }
+  // Respond early if no products were supplied in the request
+  if (productsToCheckout.length === 0) {
+    res.status(400).json({ message: "No items in the cart" });
+    return;
+  }
 
-    // Extract the references from the client request
-    const referencesArray = productsToCheckout.map((prod) => prod.reference);
+  // Extract the references from the client request
+  const referencesArray = productsToCheckout.map((prod) => prod.reference);
 
-    // Retrieve actual product details from database using references to prevent client-side manipulation of data
-    Product.find({ reference: { $in: referencesArray } })
-      .then((productList) => {
-        
-        // Initialize an array to keep track of out-of-stock products
-        let outOfStockProducts = [];
+  // Retrieve actual product details from database using references to prevent client-side manipulation of data
+  Product.find({ reference: { $in: referencesArray } })
+    .then((productList) => {
+      
+      checkStock(productList, productsToCheckout);
+     
+      const lineItems = formatLineItems(productList, productsToCheckout);
 
-        // Map each product in the productList to the line_items format required by Stripe
-        const line_items = productList.map((prod) => {
-          const { quantity } = productsToCheckout.find(
-            (item) => prod.reference === item.reference
-          );
-          
-          if(prod.stock<quantity){
-            outOfStockProducts.push(prod.name)
-            return null
-          }
-          return {
-            price_data: {
-              currency: "eur",
-              product_data: {
-                name: prod.name,
-                images: [prod.images[0]],
-              },
-              unit_amount: prod.price * 100,  // Stripe requires the price to be in the smallest currency unit (i.e., cents)
-            },
-            quantity,
-          };
-        });
-
-        // If there are out-of-stock products, send a response back to the client and return early
-        if(outOfStockProducts.length>0){
-          res.status(400).json({message:"The following products are out of stock: " + outOfStockProducts.join(", ")})
-          return
-        }
-        // Create Stripe Checkout session with the constructed line items
-        return stripe.checkout.sessions.create({
-          payment_method_types: ["card"],
-          mode: "payment",
-          line_items,
-          success_url: `${process.env.ORIGIN}/success`,
-          cancel_url: `${process.env.ORIGIN}/cancel`,
-        });
-      })
-      .then((session) => {
-        console.log("checkout session=====================>",session)
-        console.log("session.id",session.id)
-        res.json(session.url);
-      })
-      .catch(error=>{
-              
-        console.log(`An error occurred during the checkout process`,error)
-        res.status(500).json({message:"An error occured while processing your request. Please try again later"})
+      // Create Stripe Checkout session with the constructed line items
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: lineItems,
+        success_url: `${process.env.ORIGIN}/success`,
+        cancel_url: `${process.env.ORIGIN}/cancel`,
+        metadata: {
+          user_data: "probando uno dos tres",
+          product_data: JSON.stringify(productsToCheckout),
+        },
+      });
     })
-  
-   
-  });
-
-router.post('/webhook', bodyParser.raw({type: 'application/json'}), (req, res) => {
-  const sig = req.headers['stripe-signature']
-  const endpointSecret = process.env.WEBHOOK_ENDPOINT_SECRET
-  let event;
-  
-
-  try{
-    event = stripe.webhooks.constructEvent(req.body,sig,endpointSecret)
-      
-      
-    }catch(err){
-      
-      console.log("Webhook Error: ",err.message)
-      return res.status(400).send(`Webhook Error: ${err.message}`)
-  }
-
-  //Handle the event
-  switch (event.type) {
-    case 'checkout.session.completed':
-            
-      console.log('checkout session completed!!!!');
-      
-      break;
-    case 'checkout.session.expired':
-      
-      console.log('checkout session expired!!!!!');
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-  
-  // Return a 200 response to acknowledge receipt of the event
-  res.json({received: true});
+    .then((session) => {
+      console.log("checkout session=====================>", session);
+      console.log("session.id", session.id);
+      res.json(session.url);
+    })
+    .catch((error) => {
+      if (error.message) {
+        res.status(400).json({ message: error.message });
+      } else {
+        console.log(`An error occurred during the checkout process`, error);
+        res
+          .status(500)
+          .json({
+            message:
+              "An error occured while processing your request. Please try again later",
+          });
+      }
+    });
 });
 
-  
-  
-  module.exports = router
+router.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  (req, res) => {
+    const sig = req.headers["stripe-signature"];
+    const endpointSecret = process.env.WEBHOOK_ENDPOINT_SECRET;
+    let event;
+
+    // Construct the event from the raw request body and verify the signature
+    try {
+      event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+    } catch (err) {
+      console.log("Webhook Error: ", err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    //Handle the event
+    switch (event.type) {
+      case "checkout.session.completed":
+        console.log("--------------------------------");
+        const totalPaid = event.data.object.amount_total / 100;
+        console.log("amount paid", totalPaid, "€");
+        const metadata = event.data.object.metadata;
+        console.log("metadata", metadata);
+        const productData = JSON.parse(metadata.product_data);
+        console.log("product_data", productData);
+        console.log("checkout session completed!!!!");
+
+        break;
+      case "checkout.session.expired":
+        console.log("checkout session expired!!!!!");
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    res.json({ received: true });
+  }
+);
+
+module.exports = router;
