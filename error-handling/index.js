0b3e0135185c01206f67d894def72fb528c164ce
jogopin <jonn.g.pineda@gmail.com>
2023-07-27 14:43:57 +0200

    Refactor error handling in central error handler middleware Implement specific error handling functions to improve modularity. Utilize error code constants for enhanced readability. Handle duplicate key error separately to offer clearer response messages.

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,33 +1,59 @@
 const ErrorHandler = require("./ErrorHandler");
+const ERROR_CODES = {
+  INVALID_TOKEN: "invalid_token",
+  CREDENTIALS_REQUIRED: "credentials_required",
+  DUPLICATED_KEY: 11000
+};
+
+function logError(req, error) {
+  console.error("ERROR", req.method, req.path, error);
+}
+
+function handleInvalidTokenError(req, res) {
+  logError(req, "Unauthorized");
+  return res.status(401).json({ message: "Unauthorized token" });
+}
+
+function handleCredentialsRequiredError(req, res) {
+  logError(req, "Missing credentials");
+  return res.status(401).json({ message: "Missing credentials" });
+}
+
+function handleDuplicatedKeyError(err, res) {
+  const key = Object.keys(err.keyValue)[0];
+  const value = err.keyValue[key];
+  return res.status(422).json({message:`The "${key}" "${value}" is already used`});
+}
 
 module.exports = (app) => {
+  // this middleware runs whenever requested page is not available
   app.use((req, res, next) => {
-    // this middleware runs whenever requested page is not available
     res.status(404).json({ message: "This route does not exist" });
   });
 
+  // whenever you call next(err), this middleware will handle the error
   app.use((err, req, res, next) => {
-    // whenever you call next(err), this middleware will handle the error
-    // always logs the error
-    if(err.code === "invalid_token") {
-      console.log("Unauthorized",req.method,req.path)
-      
-      return res.status(401).json({ message: 'Unauthorized token' });
-      
-    } else if (err instanceof ErrorHandler) { 
-      console.error("ERROR", req.method, req.path, err.message);
-      return res.status(err.statusCode).json({ message: err.message }); // return status code and message from ErrorHandler
-    }
-
-
-    console.error("ERROR", req.method, req.path, err);
     
-
-    // only render if the error ocurred before sending the response
-    if (!res.headersSent) {
-      return res.status(500).json({
-        message: "Internal server error. please try again later",
-      });
+    switch(err.code) {
+      case ERROR_CODES.INVALID_TOKEN:
+        return handleInvalidTokenError(req, res);
+      case ERROR_CODES.CREDENTIALS_REQUIRED:
+        return handleCredentialsRequiredError(req, res);
+      case ERROR_CODES.DUPLICATED_KEY:
+        return handleDuplicatedKeyError(err, res);
+      default:
+        if (err instanceof ErrorHandler) {
+          logError(req, err.message);
+          return res.status(err.statusCode).json({ message: err.message });
+        }
+        logError(req, err);
+        //if the error ocurred before sending the response
+        if (!res.headersSent) {
+          return res.status(500).json({
+            message: "Internal server error. please try again later",
+          });
+        }
     }
   });
 };
+
