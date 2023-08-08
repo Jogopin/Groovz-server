02a490e145fb72be0c7d3bef4ed260e46bfd639a
jogopin <jonn.g.pineda@gmail.com>
2023-08-08 12:24:51 +0200

    improve: Update rate limit message with ErrorHandler

--- middleware/limiter.middleware.js
+++ middleware/limiter.middleware.js
@@ -1,9 +1,10 @@
 const rateLimit = require("express-rate-limit");
+const ErrorHandler = require("../error-handling/ErrorHandler");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // limit each IP to 5 requests per windowMs
-  message: "Too many requests from this IP, please try again after 15 minutes"
+  max: 3, // limit each IP to 3 requests per windowMs
+  message: new ErrorHandler(429,"Too many requests from this IP, please try again after 15 minutes")
 });
 
 module.exports={limiter}
